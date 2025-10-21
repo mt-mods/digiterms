5b4bd080ae08fc32668147b778a7dc7c7232d1d5
maplemedley <198984680+maplemedley@users.noreply.github.com>
2025-10-21 19:00:34 +0200

    Use xcompat for recipes

--- crafts.lua
+++ crafts.lua
@@ -20,92 +20,100 @@
 
 local function material_fallback(list)
   for _, material in ipairs(list) do
-    if minetest.registered_items[material] then
+    if core.registered_items[material] then
       return material
     end
   end
+  return nil
 end
 
 local body = material_fallback({
   'homedecor:plastic_sheeting',
-  'default:steel_ingot',
+  xcompat.materials.steel_ingot,
 })
 
 local glass = material_fallback({
   'xpanes:pane_flat',
-  'default:glass',
+  xcompat.materials.glass,
 })
 
 local electronic = material_fallback({
   'mesecons_microcontroller:microcontroller0000',
   'mesecons_luacontroller:luacontroller0000',
-  'default:copper_ingot',
+  xcompat.materials.copper_ingot,
+  xcompat.materials.gold_ingot,
 })
 
 local button = material_fallback({
   'mesecons_button:button_off',
-  'default:stone',
+  'mcl_buttons:button_stone_off',
+  xcompat.materials.stone,
+})
+
+local silicon = material_fallback({
+	'mesecons_materials:silicon',
+	xcompat.materials.dye_black,
 })
 
 local wire = 'digilines:wire_std_00000000'
 
-minetest.register_craft({
-	output = "digiterms:lcd_monitor 1",
+core.register_craft({
+	output = "digiterms:lcd_monitor",
 	recipe = {
 		{body, electronic, ''},
-		{glass, material_fallback({'mesecons_materials:silicon', 'dye:black'}), ''},
-		{body, wire, ''}
+		{glass, silicon, ''},
+		{body, wire, ''},
 	}
 })
 
-minetest.register_craft({
+core.register_craft({
 	output = "digiterms:cathodic_beige_monitor",
 	recipe = {
-		{body, body, 'dye:yellow'},
-		{glass, 'dye:orange', electronic},
+		{body, body, xcompat.materials.dye_yellow},
+		{glass, xcompat.materials.dye_orange, electronic},
 		{body, body, wire}
 	}
 })
 
-minetest.register_craft({
+core.register_craft({
 	output = "digiterms:cathodic_white_monitor",
 	recipe = {
-		{body, body, 'dye:white'},
-		{glass, 'dye:green', electronic},
+		{body, body, xcompat.materials.dye_white},
+		{glass, xcompat.materials.dye_green, electronic},
     {body, body, wire}
 	}
 })
 
-minetest.register_craft({
+core.register_craft({
 	output = "digiterms:cathodic_black_monitor",
 	recipe = {
-		{body, body, 'dye:black'},
-		{glass, 'dye:white', electronic},
+		{body, body, xcompat.materials.dye_black},
+		{glass, xcompat.materials.dye_white, electronic},
     {body, body, wire}
 	}
 })
 
 
-minetest.register_craft({
+core.register_craft({
 	output = "digiterms:beige_keyboard",
 	recipe = {
-		{button, button, 'dye:yellow'},
+		{button, button, xcompat.materials.dye_yellow},
     {body, body, wire}
 	}
 })
 
-minetest.register_craft({
+core.register_craft({
 	output = "digiterms:white_keyboard",
 	recipe = {
-		{button, button, 'dye:white'},
+		{button, button, xcompat.materials.dye_white},
     {body, body, wire}
 	}
 })
 
-minetest.register_craft({
+core.register_craft({
 	output = "digiterms:black_keyboard",
 	recipe = {
-		{button, button, 'dye:black'},
+		{button, button, xcompat.materials.dye_black},
     {body, body, wire}
 	}
 })
